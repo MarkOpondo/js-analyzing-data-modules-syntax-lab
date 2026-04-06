ffbf5934c8029a64786bd66aa0401f7992e5150d
Mark Opondo <markopondoi.t@gmail.com>
2026-04-06 21:47:04 +0300

    One test left

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+require('datejs')
+
+function combineUsers(...args)  {
+  const combinedObject = {
+    users: []
+  }
+
+  for (const arg in args) {
+    // const combinedUsers = [combinedObject.users, ...arg]
+    combinedObject.users.push(...arg)
+    // combinedObject.users = combinedUsers
+  }
+
+  const merge_date = new Date().toString('M/d/yyyy')
+  combinedObject.merge_date = merge_date
+
+  return combinedObject
+}
+
 
 
 
